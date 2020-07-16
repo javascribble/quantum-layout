a2f877a759fdfe450c32265e75a6cda67715d82a
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-16 13:42:52 -0400

    update template

--- source/elements/layout.js
+++ source/elements/layout.js
@@ -1,11 +1,12 @@
-import { Component } from '../../references/quantum.js';
+import { Component, template } from '../../references/quantum.js';
+import html from '../templates/layout.js';
 
 export class Layout extends Component {
     constructor() {
         super();
     }
 
-    static template = document.querySelector('#quantum-layout');
+    static template = template(html);
 
     static attributes = [];
 }
