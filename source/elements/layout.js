92e6720aaa18c33bc28ef0605fbc11dcf1265b85
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-12-15 12:17:39 -0500

    remove empty constructor

--- source/elements/layout.js
+++ source/elements/layout.js
@@ -2,10 +2,6 @@ import { Component, template, define } from '../import.js';
 import html from '../templates/layout.js';
 
 export class Layout extends Component {
-    constructor() {
-        super();
-    }
-
     static template = template(html);
 
     static get observedAttributes() { return ['stack', 'lock']; }
