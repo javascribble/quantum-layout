1a4c647bbaa2c65b2206b72fe6938bca5c2adaf7
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-12-22 11:38:03 -0500

    looking into drop regions

--- source/elements/layout.js
+++ source/elements/layout.js
@@ -2,36 +2,14 @@ import { Component, template, define } from '../import.js';
 import html from '../templates/layout.js';
 
 export class Layout extends Component {
-    constructor() {
-        super();
-
-        this.addEventListener('dragover', this.#dragOver);
-    }
-
     static template = template(html);
 
-    static get observedAttributes() { return ['stack', 'lock', 'persist']; }
-
-    persistAttributeChanged(attribute, previousValue, currentValue) {
-
-    }
+    static get observedAttributes() { return ['stack', 'lock']; }
 
     defaultSlotChanged(slot, addedElements, deletedElements, currentElements) {
         if (currentElements.length === 0) {
             this.remove();
         }
-    }
-
-    #dragOver(event) {
-
-    }
-
-    serialize() {
-
-    }
-
-    deserialize(data) {
-
     }
 }
 
