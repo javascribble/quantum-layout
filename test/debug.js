0fca446675194badbdeed5d76d5fc788a00e2450
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-06-09 15:44:33 -0400

    update build

--- test/debug.js
+++ test/debug.js
@@ -1,5 +1,8 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/node_modules/@javascribble/quantum-tabs/source/main.js';
+import '/source/extensions/storage.js';
 import '/source/main.js';
 
-document.body.style.visibility = 'visible'; 
+const layout = document.querySelector("#root");
+
+document.body.style.visibility = 'visible';
