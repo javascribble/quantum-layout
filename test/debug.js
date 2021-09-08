fb6f70f9c6d2211d7bc6ab61f2b5f52c4db4c48c
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-08 08:20:48 -0400

    remove import

--- test/debug.js
+++ test/debug.js
@@ -1,8 +1,5 @@
 import '/node_modules/@javascribble/quantum/bundles/main-window.js';
 import '/node_modules/@javascribble/quantum-tabs/bundles/main.js';
 import '/bundles/main.js';
-import '/bundles/main-extensions.js';
-
-const layout = document.querySelector("#root");
 
 document.body.style.visibility = 'visible';
